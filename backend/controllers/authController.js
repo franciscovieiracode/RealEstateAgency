53ff3e60f289aec851a20e634aa6ec5b626b1133
FRANCISCO JOAO MOREIRA VIEIRA <8170464@estg.ipp.pt>
2022-06-09 08:15:49 +0100

    Removed backend

--- backend/controllers/authController.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var User = require('../models/user')
-
-var authController = {}
-
-//
-authController.login = function(req, res){
-    
-}
--- backend/routes/auth.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var authController = require('../controllers/authController')
-
-router.get('/api/v1/user/regist', authController.login);
-
-module.exports = router;
